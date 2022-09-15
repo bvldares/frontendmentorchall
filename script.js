031e335374515ca40085bc3cd8f85d070fac9928
Nunzio Baldares <bvldares@outlook.com>
2022-09-15 20:22:04 +0200

    bro

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const dropdown = document.getElementsByClassName("nav-list-btn")
 const dropContent = document.getElementsByClassName("links")
+const navBar = document.querySelector("nav")
+
+
 for(let i=0; i<dropdown.length; i++){
     dropdown[i].addEventListener("click", function(){
         if(dropContent[i].style.display === "block"){
@@ -12,3 +15,10 @@ for(let i=0; i<dropdown.length; i++){
 
 
 
+document.querySelector(".nav-open").addEventListener("click", function(){
+    if(navBar.style.transform === "translateX(100%)"){
+        navBar.style.transform = "translateX(0%)"
+    }else{
+        navBar.style.transform = "translateX(100%)"
+    }
+})
